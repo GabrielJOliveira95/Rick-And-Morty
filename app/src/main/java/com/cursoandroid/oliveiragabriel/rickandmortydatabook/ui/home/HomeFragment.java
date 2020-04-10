5725919e5a1879a26ad6eaebe5ca92d51fe695e0
Gabriel Oliveira <gabrieljoliveira95@gmail.com>
2020-04-10 15:59:25 -0300

    Add slide on main background using ViewFlipper

--- app/src/main/java/com/cursoandroid/oliveiragabriel/rickandmortydatabook/ui/home/HomeFragment.java
+++ app/src/main/java/com/cursoandroid/oliveiragabriel/rickandmortydatabook/ui/home/HomeFragment.java
@@ -4,31 +4,47 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.cursoandroid.oliveiragabriel.rickandmortydatabook.R;
 
 public class HomeFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
+
+    private ViewFlipper viewFlipper;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-        homeViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
 
-            }
-        });
-        return root;
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        int image[] = {R.drawable.fundo0, R.drawable.fundo1jpg, R.drawable.fundo2jpg, R.drawable.fundo3jpg, R.drawable.fundo4jpg, R.drawable.fundo5};
+        viewFlipper = view.findViewById(R.id.viewflipper);
+
+
+        for (int item: image){
+
+            flipperView(item);
+        }
+
+        return view;
     }
+
+    public void flipperView(int image){
+
+        ImageView imageView = new ImageView(getActivity());
+        imageView.setBackgroundResource(image);
+
+        viewFlipper.addView(imageView);
+        viewFlipper.setFlipInterval(5000);
+        viewFlipper.setAutoStart(true);
+
+        viewFlipper.setInAnimation(getActivity(), android.R.anim.slide_in_left);
+        viewFlipper.setOutAnimation(getActivity(), android.R.anim.slide_out_right);
+    }
+
 }
